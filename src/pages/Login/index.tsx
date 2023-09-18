6823e138aba08ac3971628df55140f8eb534f5f1
Italo Santos <t_idsantos@sfiec.org.br>
2023-09-18 09:31:37 -0300

    Feat: Create login screen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,132 @@
+import { useContext, useState } from "react";
+import {
+  Button,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Paper,
+  TextField,
+  Toolbar,
+} from "@mui/material";
+import {
+  Visibility as VisibilityIcon,
+  VisibilityOff as VisibilityOffIcon,
+} from "@mui/icons-material";
+import { Formik, Form as FormikForm } from "formik";
+
+import { successMessage } from "../../components/Messages";
+import { UserContext } from "../../contexts/UserContext";
+import { InputMask } from "../../components/InputMask";
+
+import { useStyles } from "./styles";
+
+interface LoginData {
+  cpf: string;
+  cnpj: string;
+  password: string;
+}
+
 export const Login = () => {
-  return <div>Login</div>;
+  const { classes } = useStyles();
+  const { setIsSigned } = useContext(UserContext);
+
+  const loginData: LoginData = {
+    cpf: "",
+    cnpj: "",
+    password: "",
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const fakeLogin = (values: LoginData) => {
+    localStorage.clear();
+    localStorage.setItem("signed", JSON.stringify(true));
+    setIsSigned(true);
+    successMessage("Login realizado com sucesso!");
+  };
+
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={loginData}
+      onSubmit={(values) => {
+        fakeLogin(values);
+      }}
+    >
+      {({ handleChange, values, handleSubmit }) => (
+        <FormikForm>
+          <Toolbar />
+          <Grid container>
+            <Grid item lg={12} className={classes.formContainer}>
+              <Paper className={classes.paper}>
+                <div className={classes.loginContentContainer}>
+                  <div style={{ marginTop: 12 }}>
+                    <InputLabel className={classes.inputLabel}>CPF</InputLabel>
+                    <TextField
+                      name="cpf"
+                      value={values.cpf}
+                      onChange={handleChange}
+                      placeholder="Insira aqui seu CPF"
+                      variant="outlined"
+                      size="small"
+                      fullWidth
+                      required
+                      InputProps={{
+                        inputComponent: InputMask as any,
+                      }}
+                    />
+                  </div>
+
+                  <div style={{ marginTop: 12 }}>
+                    <InputLabel className={classes.inputLabel}>
+                      Senha
+                    </InputLabel>
+                    <TextField
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Senha"
+                      variant="outlined"
+                      size="small"
+                      fullWidth
+                      required
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={() =>
+                                setShowPassword((prevState) => !prevState)
+                              }
+                              edge="end"
+                            >
+                              {showPassword ? (
+                                <VisibilityIcon />
+                              ) : (
+                                <VisibilityOffIcon />
+                              )}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </div>
+
+                  <Button
+                    className={classes.buttonLogin}
+                    onClick={() => handleSubmit()}
+                    fullWidth
+                  >
+                    Entrar
+                  </Button>
+                </div>
+              </Paper>
+            </Grid>
+          </Grid>
+        </FormikForm>
+      )}
+    </Formik>
+  );
 };
---
+++ src/pages/Login/styles.tsx
@@ -0,0 +1,60 @@
+import { makeStyles } from "tss-react/mui";
+
+export const useStyles = makeStyles()((theme) => {
+  return {
+    formContainer: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      height: "90vh",
+    },
+    paper: {
+      color: theme.palette.text.secondary,
+      marginTop: 12,
+    },
+    loginContentContainer: {
+      display: "flex",
+      width: 400,
+      padding: 24,
+      flexDirection: "column",
+    },
+    inputLabel: {
+      marginBottom: theme.spacing(1),
+      color: "#14532D",
+      fontWeight: 600,
+    },
+    buttonLogin: {
+      fontSize: 14,
+      fontWeight: "bold",
+      marginTop: 24,
+      padding: "12px 30px",
+      color: "#FFF",
+      textTransform: "capitalize",
+      transition: ".5s ease",
+      backgroundColor: "#9c27b0",
+      "&:hover": {
+        backgroundColor: "rgba(156,39,176,0.8)",
+      },
+      borderRadius: 5,
+      textDecoration: "none",
+    },
+    resetPasswdText: {
+      marginLeft: 12,
+      fontSize: 14,
+      color: "#9c27b0",
+      cursor: "pointer",
+      "&:hover": {
+        color: "rgba(156,39,176,0.8)",
+      },
+    },
+    resetPasswdContainer: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 12,
+    },
+  };
+});
