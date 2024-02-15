d3bed53038bcab21434e03e4f4ecfe3c1447dfa6
lmferreira <lmferreira@sfiec.org.br>
2024-02-15 18:32:49 -0300

    update: list clients related works

--- src/screens/Clients/ListClientsRelatedWorks/index.tsx
+++ src/screens/Clients/ListClientsRelatedWorks/index.tsx
@@ -1,80 +1,128 @@
 import { useEffect, useMemo, useState } from "react";
-
-import { useNavigate } from "react-router-dom";
-import { MRT_ColumnDef } from "material-react-table";
-import { useDebounce } from "use-debounce";
-
-import { Table } from "../../../components/Table";
-
-import { Grid } from "@mui/material";
-import { useStyles } from "./styles";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from "material-react-table";
+import {
+  Checkbox,
+  Chip,
+  Grid,
+  Paper,
+  darken,
+  lighten,
+  useTheme,
+} from "@mui/material";
 import { useClients } from "../../../hooks/useClients";
-import { Client } from "../../../types";
-
-type ClientsRelatedWorksTableItem = Partial<Client>;
+import { TitleScreen } from "../../../components/TitleScreen";
+import { useStyles } from "./styles";
+import { useNavigate } from "react-router-dom";
+import { EditIcon } from "../../../components/EditIcon";
+import { TablePagination } from "../../../components/Table/Pagination";
+import { ModalDisable } from "../../../components/Table/ModalDisable";
+import { Delete } from "@mui/icons-material";
+import { BackgroundAvatar } from "../../../components/Avatar";
 
 export const ListClientsRelatedWorks = () => {
   const { classes } = useStyles();
-
   const navigate = useNavigate();
   const {
-    getAllClientsRelatedWorks,
     listClientsRelatedWorks,
-    disableClient,
-    getClientBySearch,
+    getAllClientsRelatedWorks,
     pagination,
     handleChangePagination,
   } = useClients();
+  const theme = useTheme();
 
-  const [selectedClientId, setselectedClientId] = useState<number>(0);
+  const [selectedClientRelatedWorksId, setselectedClientRelatedWorksId] =
+    useState<number>(0);
   const [modalOpen, setIsModalOpen] = useState(false);
 
   const handleClose = () => {
     setIsModalOpen(false);
   };
 
-  const handleDisable = () => {
-    disableClient(selectedClientId);
-    setIsModalOpen(false);
-  };
-
-  const [text, setText] = useState("");
-  const [value] = useDebounce(text, 1000);
+  //light or dark green
+  const baseBackgroundColor =
+    theme.palette.mode === "dark" ? "#FFFFFF" : "#FFFFFF";
 
   useEffect(() => {
-    if (value) {
-      getClientBySearch(value);
-    } else {
-      getAllClientsRelatedWorks();
-    }
-  }, [value]);
+    getAllClientsRelatedWorks();
+  }, []);
 
-  const columns = useMemo<MRT_ColumnDef<ClientsRelatedWorksTableItem>[]>(
+  const columns = useMemo<MRT_ColumnDef<any>[]>(
     () => [
       {
-        accessorKey: "id",
-        header: "ID",
+        header: "Status",
+        accessorFn: (originalRow) => (originalRow.active ? "true" : "false"),
+        id: "active",
+        filterVariant: "checkbox",
+        Cell: ({ cell }) => {
+          const status = cell.getValue() === "true" ? "Ativo" : "Inativo";
+          const chipColor = status === "Ativo" ? "success" : "error";
+          return <Chip label={status} color={chipColor} />;
+        },
+        size: 170,
       },
       {
-        accessorKey: "active",
-        header: "Ativa",
-      },
-      {
-        accessorKey: "name",
+        accessorKey: "relatedWork",
+        enableColumnFilterModes: false,
+        filterFn: "startsWith",
         header: "Nome da Obra",
       },
       {
         accessorKey: "responsible",
+        enableColumnFilterModes: false,
+        filterFn: "startsWith",
         header: "Responsável",
       },
     ],
     []
   );
 
+  const table = useMaterialReactTable({
+    columns,
+    data: listClientsRelatedWorks,
+    enableColumnFilterModes: true,
+    initialState: { showColumnFilters: true },
+    filterFns: {
+      customFilterFn: (row, id, filterValue) => {
+        return row.getValue(id) === filterValue;
+      },
+    },
+    localization: {
+      filterCustomFilterFn: "Custom Filter Fn",
+    } as any,
+    muiTablePaperProps: {
+      elevation: 0,
+    },
+    muiTableBodyProps: {
+      sx: (theme) => ({
+        '& tr:nth-of-type(odd):not([data-selected="true"]):not([data-pinned="true"]) > td':
+          {
+            backgroundColor: "#FAFAFA",
+          },
+      }),
+    },
+    mrtTheme: (theme) => ({
+      baseBackgroundColor: baseBackgroundColor,
+      draggingBorderColor: theme.palette.secondary.main,
+    }),
+    enablePagination: false,
+    enableBottomToolbar: false,
+  });
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} lg={12}>
-        <Table columns={columns} data={listClientsRelatedWorks} />
+        <MaterialReactTable table={table} />
+        {Boolean(listClientsRelatedWorks.length) && (
+          <TablePagination
+            count={pagination.pageQuantity}
+            page={pagination.currentPage}
+            onChange={handleChangePagination}
+          />
+        )}
       </Grid>
     </Grid>
   );
