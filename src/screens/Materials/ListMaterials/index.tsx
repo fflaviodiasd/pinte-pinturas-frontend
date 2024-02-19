36034d30bbdfdde354abc0f482238b6ecb8b8cf7
lmferreira <lmferreira@sfiec.org.br>
2024-02-19 09:58:17 -0300

    update: list materials

--- src/screens/Materials/ListMaterials/index.tsx
+++ src/screens/Materials/ListMaterials/index.tsx
@@ -1,34 +1,38 @@
 import { useEffect, useMemo, useState } from "react";
-
-import { useNavigate } from "react-router-dom";
-import { MRT_ColumnDef } from "material-react-table";
-import { useDebounce } from "use-debounce";
-
-import { Table } from "../../../components/Table";
-
-import { Grid, Paper } from "@mui/material";
-import { Material } from "../../../types";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from "material-react-table";
+import {
+  Checkbox,
+  Grid,
+  Paper,
+  darken,
+  lighten,
+  useTheme,
+} from "@mui/material";
 import { TitleScreen } from "../../../components/TitleScreen";
 import { useStyles } from "./styles";
+import { useNavigate } from "react-router-dom";
+import { EditIcon } from "../../../components/EditIcon";
 import { TablePagination } from "../../../components/Table/Pagination";
-import { Launch } from "@mui/icons-material";
+import { ModalDisable } from "../../../components/Table/ModalDisable";
+import { Delete, Launch } from "@mui/icons-material";
 import { useMaterials } from "../../../hooks/useMaterials";
 import { ModalRegisterMaterial } from "../../../components/Modal/Materials/Register";
 
-type MaterialsTableItem = Partial<Material>;
-
 export const ListMaterials = () => {
   const { classes } = useStyles();
-
   const navigate = useNavigate();
   const {
-    getAllMaterials,
     listMaterials,
+    getAllMaterials,
     disableMaterial,
-    getMaterialBySearch,
     pagination,
     handleChangePagination,
   } = useMaterials();
+  const theme = useTheme();
 
   const [selectedMaterialId, setselectedMaterialId] = useState<number>(0);
   const [modalOpen, setIsModalOpen] = useState(false);
@@ -37,60 +41,95 @@ export const ListMaterials = () => {
     setIsModalOpen(false);
   };
 
-  const handleDisable = () => {
-    disableMaterial(selectedMaterialId);
-    setIsModalOpen(false);
-  };
-
-  const [text, setText] = useState("");
-  const [value] = useDebounce(text, 1000);
+  const baseBackgroundColor =
+    theme.palette.mode === "dark" ? "#FFFFFF" : "#FFFFFF";
 
   useEffect(() => {
-    if (value) {
-      getMaterialBySearch(value);
-    } else {
-      getAllMaterials();
-    }
-  }, [value]);
+    getAllMaterials();
+  }, []);
 
-  const columns = useMemo<MRT_ColumnDef<MaterialsTableItem>[]>(
+  const columns = useMemo<MRT_ColumnDef<any>[]>(
     () => [
       {
         id: "edit",
         header: "",
         columnDefType: "display",
-        muiTableHeadCellProps: {
-          align: "right",
-        },
-        muiTableBodyCellProps: {
-          align: "right",
-        },
-
-        Cell: ({ cell }) => <Launch />,
+        Cell: ({ cell }) => (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Launch sx={{ color: "gray" }} />
+          </div>
+        ),
       },
       {
         accessorKey: "name",
+        enableColumnFilterModes: false,
+        filterFn: "startsWith",
         header: "Nome",
       },
       {
         accessorKey: "group",
+        enableColumnFilterModes: false,
+        filterFn: "startsWith",
         header: "Grupo",
       },
       {
         accessorKey: "unit",
+        enableColumnFilterModes: false,
+        filterFn: "startsWith",
         header: "Unidade",
       },
       {
         accessorKey: "expectedConsumption",
+        enableColumnFilterModes: false,
+        filterFn: "startsWith",
         header: "Consumo Esperado",
       },
       {
         accessorKey: "consumptionRealized",
+        enableColumnFilterModes: false,
+        filterFn: "startsWith",
         header: "Consumo Realizado",
       },
     ],
     []
   );
+
+  const table = useMaterialReactTable({
+    columns,
+    data: listMaterials,
+    enableColumnFilterModes: true,
+    initialState: { showColumnFilters: true },
+    filterFns: {
+      customFilterFn: (row, id, filterValue) => {
+        return row.getValue(id) === filterValue;
+      },
+    },
+    localization: {
+      filterCustomFilterFn: "Custom Filter Fn",
+    } as any,
+    muiTablePaperProps: {
+      elevation: 0,
+    },
+    muiTableBodyProps: {
+      sx: (theme) => ({
+        '& tr:nth-of-type(odd):not([data-selected="true"]):not([data-pinned="true"]) > td':
+          {
+            backgroundColor: "#FAFAFA",
+          },
+      }),
+    },
+    mrtTheme: (theme) => ({
+      baseBackgroundColor: baseBackgroundColor,
+      draggingBorderColor: theme.palette.secondary.main,
+    }),
+    enablePagination: false,
+    enableBottomToolbar: false,
+  });
 
   return (
     <Grid container spacing={2}>
@@ -104,7 +143,7 @@ export const ListMaterials = () => {
       </Grid>
 
       <Grid item xs={12} lg={12}>
-        <Table columns={columns} data={listMaterials} />
+        <MaterialReactTable table={table} />
         {Boolean(listMaterials.length) && (
           <TablePagination
             count={pagination.pageQuantity}
