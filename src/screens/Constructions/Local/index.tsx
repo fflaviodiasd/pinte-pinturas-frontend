f1a94ec6d97de276170f5b6f86184c94ecc999de
lmferreira <lmferreira@sfiec.org.br>
2024-03-06 09:33:28 -0300

    feat: level component and dynamic columns

--- src/screens/Constructions/Local/index.tsx
+++ src/screens/Constructions/Local/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   MaterialReactTable,
   type MRT_ColumnDef,
@@ -7,75 +7,92 @@ import {
 import { Box, Button } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useMaterials } from "../../../hooks/useMaterials";
+import { LevelComponent } from "../../../components/Level";
+import { api } from "../../../services/api";
+import { useParams } from "react-router-dom";
 
 const Locations = () => {
-  const [validationErrors, setValidationErrors] = useState<
-    Record<string, string | undefined>
-  >({});
+  const [dynamicColumns, setDynamicColumns] = useState<MRT_ColumnDef<any>[]>(
+    []
+  );
+
+  const { id: levelId } = useParams();
 
   const { listMaterialGroups } = useMaterials();
 
-  const columns = useMemo<MRT_ColumnDef<any>[]>(
-    () => [
-      {
-        accessorKey: "id",
-        header: "ID",
-        enableEditing: false,
-        size: 80,
-      },
-      {
-        accessorKey: "micro",
-        header: "Micro",
-      },
-      {
-        accessorKey: "checklist",
-        header: "Checklist",
-      },
-    ],
-    [validationErrors]
-  );
+  useEffect(() => {
+    const fetchLevel = async () => {
+      try {
+        const response = await api.get(`constructions/${levelId}/level_area/`);
+        const level = response.data.results;
+        const newDynamicColumns = [
+          {
+            accessorKey: "id",
+            header: "ID",
+            enableEditing: false,
+            size: 80,
+          },
+          {
+            accessorKey: "micro",
+            header: "Micro",
+          },
+          {
+            accessorKey: "checklist",
+            header: "Checklist",
+          },
+        ];
+
+        level.forEach((level: any, index: any) => {
+          newDynamicColumns.push({
+            accessorKey: `nivel_${index}`,
+            header: level.name,
+          });
+        });
+        setDynamicColumns(newDynamicColumns);
+      } catch (error) {
+        console.error("Erro ao buscar os níveis:", error);
+      }
+    };
+
+    fetchLevel();
+  }, []);
 
   const table = useMaterialReactTable({
-    columns,
+    columns: dynamicColumns,
     data: listMaterialGroups,
-    createDisplayMode: "row", // ('modal', and 'custom' are also available)
-    editDisplayMode: "cell", // ('modal', 'row', 'table', and 'custom' are also available)
-    // enableCellActions: true,
-    //enableClickToCopy: "context-menu",
+    createDisplayMode: "row",
+    editDisplayMode: "cell",
     enableColumnPinning: true,
     enableEditing: true,
     enableRowActions: false,
     enableExpanding: true,
     enablePagination: false,
-    //enableBottomToolbar: false,
     getRowId: (row) => row.id,
     muiTableContainerProps: {
       sx: {
         minHeight: "500px",
       },
     },
-    onCreatingRowCancel: () => setValidationErrors({}),
-    //onCreatingRowSave: handleCreateUser,
+    // onCreatingRowCancel: () => setValidationErrors({}),
     renderBottomToolbarCustomActions: () => (
       <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-        <Button
-          color="primary"
-          variant="contained"
-          //onClick={handleSaveUsers}
-        >
+        <Button color="primary" variant="contained">
           Salvar
         </Button>
       </Box>
     ),
     renderTopToolbarCustomActions: ({ table }) => (
-      <Button
-        variant="contained"
-        onClick={() => {
-          table.setCreatingRow(true);
-        }}
-      >
-        Adicionar Linha
-      </Button>
+      <div>
+        <LevelComponent />
+        <Button
+          variant="contained"
+          onClick={() => {
+            table.setCreatingRow(true);
+          }}
+        >
+          Adicionar Linha
+        </Button>
+      </div>
     ),
   });
 
