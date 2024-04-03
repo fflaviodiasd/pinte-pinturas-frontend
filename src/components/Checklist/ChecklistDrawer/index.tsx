ea11e3934eb93cb683bfa16947be7e20f243fb86
lmferreira <lmferreira@sfiec.org.br>
2024-04-03 09:37:54 -0300

    update: style checklist drawer

--- src/components/Checklist/ChecklistDrawer/index.tsx
+++ src/components/Checklist/ChecklistDrawer/index.tsx
@@ -1,19 +1,37 @@
 import {
+  Button,
   Drawer,
   IconButton,
   List,
   ListItem,
   ListItemText,
+  Typography,
 } from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+const handleClose = () => {
+  alert("close");
+};
+
+const handleCopy = () => {
+  alert("copy");
+};
 
 export const ChecklistDrawer = ({ open, onClose }: any) => {
   return (
     <Drawer anchor="right" open={open} onClose={onClose}>
       <div style={{ width: 250 }}>
-        <IconButton onClick={onClose}>
-          <ChevronRightIcon />
-        </IconButton>
+        <div style={{ margin: "1rem" }}>
+          <span
+            style={{
+              fontFamily: "Open Sans",
+              fontWeight: 600,
+              fontSize: "1.125rem",
+            }}
+          >
+            Copiar Checklists
+          </span>
+        </div>
         <List>
           <ListItem>
             <ListItemText primary="Item 1" />
@@ -25,6 +43,18 @@ export const ChecklistDrawer = ({ open, onClose }: any) => {
             <ListItemText primary="Item 3" />
           </ListItem>
         </List>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button onClick={handleClose}>
+            <Typography style={{ textTransform: "capitalize" }}>
+              Cancelar
+            </Typography>
+          </Button>
+          <Button onClick={handleCopy} variant="contained">
+            <Typography style={{ textTransform: "capitalize" }}>
+              Copiar
+            </Typography>
+          </Button>
+        </div>
       </div>
     </Drawer>
   );
