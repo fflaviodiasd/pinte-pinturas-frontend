b0e85ab3cf10d8532098e742307cd353a2909bd9
lmferreira <lmferreira@sfiec.org.br>
2024-05-09 13:49:40 -0300

    update: modal checklists status visualization

--- src/components/Modal/ModalChecklists/HistoryInfo/index.tsx
+++ src/components/Modal/ModalChecklists/HistoryInfo/index.tsx
@@ -20,7 +20,7 @@ export function HistoryInfo({ checklistId }: any) {
   }, []);
 
   const STATUS_COLORS: { [key: string]: string } = {
-    "NÃO LIBERADA": "#F44336",
+    //"NÃO LIBERADA": "#F44336",
     LIBERADA: "#FF9800",
     INICIADA: "#4CAF50",
     FINALIZADA: "#2196F3",
@@ -29,28 +29,35 @@ export function HistoryInfo({ checklistId }: any) {
 
   return (
     <tbody>
-      {checklistHistory.map((item: any, index: number) => (
-        <tr key={item.histories.id}>
+      {Object.keys(STATUS_COLORS).map((status: string, index: number) => (
+        <tr key={index}>
           <td>
             <Chip
-              label={item.status}
+              label={status}
               style={{
-                backgroundColor: STATUS_COLORS[item.status],
+                backgroundColor: STATUS_COLORS[status],
                 color: "#FFFFFF",
                 fontFamily: "Open Sans",
                 fontWeight: 600,
               }}
             />
           </td>
-          <td>
-            <span>{item.histories.written_date}</span>
-          </td>
-          <td>
-            <span>{item.histories.marking_action_date}</span>
-          </td>
-          <td>
-            <span>{item.histories.responsible_action}</span>
-          </td>
+          {checklistHistory.map(
+            (item: any) =>
+              item.status === status && (
+                <>
+                  <td>
+                    <span>{item.histories.written_date}</span>
+                  </td>
+                  <td>
+                    <span>{item.histories.marking_action_date}</span>
+                  </td>
+                  <td>
+                    <span>{item.histories.responsible_action}</span>
+                  </td>
+                </>
+              )
+          )}
         </tr>
       ))}
     </tbody>
