b4bc5994dd8d5c67418fe4040b6323abb86df248
lmferreira <lmferreira@sfiec.org.br>
2024-05-28 10:43:14 -0300

    update: get list locations after delete areas

--- src/screens/Constructions/Local/index.tsx
+++ src/screens/Constructions/Local/index.tsx
@@ -248,6 +248,7 @@ const Locations = () => {
   const handleDeleteSnackbar = () => {
     selectedLocalIds.forEach(async (id) => {
       await disableConstructionLocal([id]);
+      getAllConstructionsLocations(dynamicColumns);
     });
     setSelectedLocalIds([]);
     setSnackbarOpen(false);
