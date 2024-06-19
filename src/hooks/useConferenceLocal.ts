f88f9c8b1a72e108f2abd37cb4d075bc5c101e94
Guilherme Queiroz <t_gqlima@sfiec.org.br>
2024-06-19 10:02:49 -0300

    add feature fix grouping

---
+++ src/hooks/useConferenceLocal.ts
@@ -0,0 +1,222 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-useless-catch */
+
+import { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { api } from "../services/api";
+import { UserContext } from "../contexts/UserContext";
+
+export const useConferenceLocal = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { user } = useContext(UserContext);
+
+  const [loading, setLoading] = useState(true);
+  const [listConferenceDataLocal, setListConferenceData] = useState<any[]>([]);
+  const [listMeasurementsReports, setListMeasurementsReports] = useState<any[]>([]);
+  const [listEmployeesReports, setListEmployeesReports] = useState<any[]>([]);
+  const [listTeamsReports, setListTeamsReports] = useState<any[]>([]);
+  const [listProductionData, setListProductionData] = useState<any[]>([]);
+  const [listReportsWithMeasurements, setListReportsWithMeasurements] = useState<any[]>([]);
+  const [listReportsWithEmployees, setListReportsWithEmployees] = useState<any[]>([]);
+    const [listGeneralReports, setListGeneralReports] = useState<any[]>([]); 
+    const getConferenceDataLocal = async (measurementId: string, disjunction: string) => {
+
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.get(`/reports_conference/${id}/measurements/?measurement=${measurementId}&disjunction=${disjunction}`);
+      console.log("data:", data);
+      setListConferenceData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao obter serviços de construção:", error);
+      setLoading(false);
+    }
+  };
+
+  const fetchMeasurements = async () => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.post(`/reports_conference/production/filter/`, { construction: id });
+      console.log("Measurements data:", data);
+      setListMeasurementsReports(data.measurement);
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao obter medições:", error);
+      setLoading(false);
+    }
+  };
+
+  const fetchEmployees = async (measurementId: number) => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.post(`/reports_conference/production/filter/`, { construction: id, measurement: measurementId });
+      console.log("Employees data:", data);
+      setListEmployeesReports(data.employees);
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao obter funcionários:", error);
+      setLoading(false);
+    }
+  };
+
+  const fetchTeams = async (measurementId: string, employeeId: number) => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.post(`/reports_conference/production/filter/`, {
+        construction: id,
+        measurement: measurementId,
+        employee: employeeId
+      });
+      console.log("Teams data:", data);
+      setListTeamsReports(data.teams); 
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao obter times:", error);
+      setLoading(false);
+    }
+  };
+
+  const getProductionData = async () => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return [];
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.get(`/reports_conference/${id}/production/`);
+      console.log("Production data:", data);
+      setListProductionData(data);
+      setLoading(false);
+      return data;
+    } catch (error) {
+      console.error("Erro ao obter dados de produção:", error);
+      setLoading(false);
+      return [];
+    }
+  };
+
+  const getReportsWithMeasurement = async (measurementId: number) => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return [];
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.get(`/reports_conference/${id}/production/?measurement=${measurementId}`);
+      console.log("Reports with Measurement data:", data);
+      setListReportsWithMeasurements(data);
+      setLoading(false);
+      return data;
+    } catch (error) {
+      console.error("Erro ao obter relatórios com medições:", error);
+      setLoading(false);
+      return [];
+    }
+  };
+
+  const getReportsWithEmployee = async (employeeId: number) => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return [];
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.get(`/reports_conference/${id}/production/?employee=${employeeId}`);
+      console.log("Reports with Employee data:", data);
+      setListReportsWithEmployees(data);
+      setLoading(false);
+      return data;
+    } catch (error) {
+      console.error("Erro ao obter relatórios com medições:", error);
+      setLoading(false);
+      return [];
+    }
+  };
+
+  const getReportsWithTeams = async (employeeId: any, teamId: number) => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return [];
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.get(`/reports_conference/${id}/production/?employee=${employeeId}&?team=${teamId}`);
+      console.log("Reports with Employee data:", data);
+      setListReportsWithEmployees(data);
+      setLoading(false);
+      return data;
+    } catch (error) {
+      console.error("Erro ao obter relatórios com medições:", error);
+      setLoading(false);
+      return [];
+    }
+  };
+
+  const getGeneralReports = async () => {
+    if (!id) {
+      console.error("ID da construção não foi fornecido");
+      return [];
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.get(`/reports_conference/${id}/general/`);
+      console.log("General reports data:", data);
+      setListGeneralReports(data);
+      setLoading(false);
+      return data;
+    } catch (error) {
+      console.error("Erro ao obter relatórios gerais:", error);
+      setLoading(false);
+      return [];
+    }
+  };
+
+
+  return {
+    loading,
+    setLoading,
+    getConferenceDataLocal,
+    listConferenceDataLocal,
+    fetchMeasurements,
+    fetchEmployees,
+    fetchTeams,
+    getProductionData,
+    listMeasurementsReports,
+    listEmployeesReports,
+    listTeamsReports,
+    listProductionData,
+    getReportsWithMeasurement,
+    listReportsWithMeasurements,
+    getReportsWithEmployee,
+    listReportsWithEmployees,
+    getGeneralReports, 
+    getReportsWithTeams,
+    listGeneralReports
+  };
+};
