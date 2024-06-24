0f01467feee8419563e666b0ea33b2eb84e46715
Italo Santos <italofsan@outlook.com>
2024-06-24 20:34:27 -0300

    Fix: Employee screen padding on construction screen

--- src/screens/Constructions/Indicators/Details/styles.tsx
+++ src/screens/Constructions/Indicators/Details/styles.tsx
@@ -2,6 +2,11 @@ import { makeStyles } from "tss-react/mui";
 
 export const useStyles = makeStyles()((theme) => {
   return {
+    container: {
+      marginLeft: 12,
+      marginRight: 12,
+      paddingBottom: theme.spacing(2),
+    },
     paper: {
       color: theme.palette.text.secondary,
       flexGrow: 1,
